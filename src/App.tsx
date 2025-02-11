b00f5b55fcb6e7539ac95fc7c2198e105537b87e
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2025-02-11 21:37:45 +0200

    Added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { ButtonPrimary } from "./components/Buttons";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <>
-      <ButtonPrimary>primary</ButtonPrimary>
+      <ButtonPrimary onClick={() => console.log("Clicked")}>
+        primary
+      </ButtonPrimary>
     </>
   );
 }
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -3,15 +3,24 @@ import { Button } from "@/components/ui/button";
 
 interface Props {
   children: string;
+  onClick: () => void;
 }
-export function ButtonPrimary({ children }: Props) {
-  return <Button>{children}</Button>;
-}
-
-export function ButtonSecondary({ children }: Props) {
-  return <Button variant="secondary">{children}</Button>;
+export function ButtonPrimary({ children, onClick }: Props) {
+  return <Button onClick={onClick}>{children}</Button>;
 }
 
-export function ButtonDanger({ children }: Props) {
-  return <Button variant="destructive">{children}</Button>;
+export function ButtonSecondary({ children, onClick }: Props) {
+  return (
+    <Button onClick={onClick} variant="secondary">
+      {children}
+    </Button>
+  );
+}
+
+export function ButtonDanger({ children, onClick }: Props) {
+  return (
+    <Button onClick={onClick} variant="destructive">
+      {children}
+    </Button>
+  );
 }
